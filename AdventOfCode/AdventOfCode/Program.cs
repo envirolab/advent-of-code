85a4aea3d6431a3c8e7854fb70f0d69b3235db50
Matt Duncan <matt_duncan@sbcglobal.net>
2020-12-02 11:34:41 -0500

    Completed Day1 (Part 1 and 2)

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -9,33 +9,81 @@ namespace AdventOfCode
 
         static void Main(string[] args)
         {
+            Day1Part1();
+            Day1Part2();
+        }
+
+        static void Day1Part1()
+        {
             FileStream fs = new FileStream(@"C:\Users\medun\source\repos\advent-of-code\AdventOfCode\AdventOfCode\Day1.txt", FileMode.Open, FileAccess.Read);
             StreamReader sr = new StreamReader(fs);
             string line;
             ArrayList alNumbers = new ArrayList();
             while ((line = sr.ReadLine()) != null)
             {
-                alNumbers.Add(line);
+                alNumbers.Add(Convert.ToInt32(line.Trim()));
             }
             int[] Numbers = (int[])alNumbers.ToArray(typeof(int));
+
             int Index = 0;
-            int iFirstVal = 0;
-            int iSecondVal = 0;
-            while (Index < Numbers.Length-1)
+            bool bContinue = true;
+            while (Index < Numbers.Length - 1)
             {
                 foreach (int i in Numbers)
                 {
                     if (i + Numbers[Index] == 2020)
                     {
-                        iFirstVal = i;
-                        iSecondVal = Numbers[Index];
+                        Console.WriteLine("------Day 1 Part 1------");
+                        Console.WriteLine("Numbers are " + i + " and " + Numbers[Index]);
+                        Console.WriteLine("The multiplied value of the numbers is " + i * Numbers[Index]);
+                        bContinue = false;
                         break;
                     }
-                    else
-                        Index++;
                 }
+                if (!bContinue)
+                    break;
+                else
+                    Index++;
             }
-            Console.WriteLine("Numbers are "+iFirstVal + " and " + iSecondVal);
+        }
+
+        static void Day1Part2()
+        {
+            FileStream fs = new FileStream(@"C:\Users\medun\source\repos\advent-of-code\AdventOfCode\AdventOfCode\Day1.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string line;
+            ArrayList alNumbers = new ArrayList();
+            while ((line = sr.ReadLine()) != null)
+            {
+                alNumbers.Add(Convert.ToInt32(line.Trim()));
+            }
+            int[] Numbers = (int[])alNumbers.ToArray(typeof(int));
+
+            int Index = 0;
+            bool bContinue = true;
+            while (Index < Numbers.Length - 1)
+            {
+                foreach (int i in Numbers)
+                {
+                    foreach (int j in Numbers)
+                    {
+                        if (i + j + Numbers[Index] == 2020)
+                        {
+                            Console.WriteLine("------Day 1 Part 2------");
+                            Console.WriteLine("Numbers are " + i + " and " + j + " and " + Numbers[Index]);
+                            Console.WriteLine("The multiplied value of the numbers is " + i * j * Numbers[Index]);
+                            bContinue = false;
+                            break;
+                        }
+                    }
+                    if (!bContinue)
+                        break;
+                }
+                if (!bContinue)
+                    break;
+                else
+                    Index++;
+            }
         }
     }
 }
