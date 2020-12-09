8822a7458a2ebefc589b50e4b47be8fcd7cc7b33
Matt Duncan <matt_duncan@sbcglobal.net>
2020-12-09 11:41:27 -0500

    Day 4 (Part 1)

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -12,7 +12,8 @@ namespace AdventOfCode
             //Day1Part1();
             //Day1Part2();
             //Day2Part1();
-            Day2Part2();
+            //Day2Part2();
+            Day4Part1();
         }
 
         static void Day1Part1()
@@ -193,6 +194,70 @@ namespace AdventOfCode
             {
                 Console.WriteLine(pwd);
             }
+        }
+
+        static void Day4Part1()
+        {
+            FileStream fs = new FileStream(@"C:\Users\medun\source\repos\advent-of-code\AdventOfCode\AdventOfCode\Day4.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            string[] saReqFields = new string[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+            string[] saOptFields = new string[] { "cid" };
+
+            string line = "";
+            string sPassport = "";
+            int iValidPassportCount = 0;
+            ArrayList alPassports = new ArrayList();
+      
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.Equals(""))
+                {
+                    //string[] saPassportFields = sPassport.Trim().Split(" ");
+                    alPassports.Clear();
+                    alPassports.AddRange(sPassport.Trim().Split(" "));
+                    if (alPassports.Count == 8)
+                    {
+                        iValidPassportCount++;
+                        sPassport = "";
+                    }
+                    else if (alPassports.Count == 7)
+                    {
+                        int iValidPassportFieldCount = 0;
+                        foreach (string PassportField in alPassports)
+                        {
+                            foreach (string ReqField in saReqFields)
+                            {
+                                if (ReqField.Equals(PassportField.ToLower().Substring(0, PassportField.IndexOf(":"))))
+                                {
+                                    iValidPassportFieldCount++;
+                                    sPassport = "";
+                                    break;
+                                }
+                            }
+                        }
+                        if (iValidPassportFieldCount >= 7)
+                        {
+                            iValidPassportCount++;
+                            sPassport = "";
+                        }
+                    }
+                    else
+                    {
+                        sPassport = "";
+                    }
+                }
+                else
+                {
+                    sPassport += line.Trim() + " ";
+                }
+
+;
+            }
+            sr.Close();
+            fs.Close();
+
+            Console.WriteLine("The number of valid passports found was:" + iValidPassportCount);            
         }
     }
 }
